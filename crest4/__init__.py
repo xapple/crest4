1bc1cb5e9c992702c4d14ce807a519a606e760ec
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-06-20 22:57:31 +0200

    Update __init__.py

--- crest4/__init__.py
+++ crest4/__init__.py
@@ -13,4 +13,4 @@ __version__ = '4.0.14'
 project_url = 'https://github.com/xapple/crest4'
 
 # Expose our main object at the module level #
-from crest4.classify import Classify
+from .classify import Classify
