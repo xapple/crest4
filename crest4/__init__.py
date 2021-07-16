05073e24602bba3dc8b46a98e710108c2c22a617
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-07-16 14:35:15 +0200

    Version 4.1.9

--- crest4/__init__.py
+++ crest4/__init__.py
@@ -7,7 +7,7 @@ DNA sequences obtained from environmental sequencing.
 """
 
 # Special variables #
-__version__ = '4.1.8'
+__version__ = '4.1.9'
 
 # Constants #
 project_url = 'https://github.com/xapple/crest4'
