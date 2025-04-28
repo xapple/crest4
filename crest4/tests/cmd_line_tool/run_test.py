fcf9158a5a170c268a3ace1239991190228bae8c
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2025-04-28 17:25:10 +0200

    This test is switched to subprocess because sh was not working with _return_cmd=True

--- crest4/tests/cmd_line_tool/run_test.py
+++ crest4/tests/cmd_line_tool/run_test.py
@@ -6,12 +6,13 @@ Script to run the `cmd_line_tool` integration test.
 """
 
 # Built-in modules #
-import inspect, sys
+import inspect, sys, subprocess
 
 # First party modules #
 from autopaths import Path
 
 # Third party modules #
+import pytest, asyncio
 
 # Internal modules #
 
@@ -20,26 +21,34 @@ this_file = Path((inspect.stack()[0])[1])
 this_dir  = this_file.directory
 
 ###############################################################################
-def test_cmd_line_tool():
+@pytest.mark.asyncio
+async def test_cmd_line_tool():
     # The input fasta #
     fasta = this_dir.find('*.fasta')
     # The output directory #
     output_dir = this_dir + 'results/'
     output_dir.remove()
-    # Get the path of the current python executable #
-    from crest4 import sh
-    this_python = sh.Command(sys.executable)
-    # Call via the command line tool #
-    result = this_python('-m',            'crest4',
-                         '--fasta',       fasta,
-                         '--output_dir',  output_dir,
-                         '--num_threads', 'True')
+    # Build the command as a list of arguments
+    cmd = [sys.executable,
+           '-m', 'crest4',
+           '--fasta', fasta,
+           '--output_dir', output_dir,
+           '--num_threads', 'True']
+    # Run the command asynchronously
+    process = await asyncio.create_subprocess_exec(*cmd,
+                                                   stdout = subprocess.PIPE,
+                                                   stderr = subprocess.PIPE)
+    # Wait for the process to finish #
+    stdout, stderr = await process.communicate()
     # Check that the results were created #
     created_file = output_dir + 'assignments.txt'
     assert created_file
+    # Check that the return code is 0 #
+    assert process.returncode == 0
     # Return #
-    return result
+    return process
 
 ###############################################################################
 if __name__ == '__main__':
-    command = test_cmd_line_tool()
+    process = test_cmd_line_tool()
+    asyncio.run(process)
