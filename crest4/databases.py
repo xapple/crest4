ad135620c3d12df2eda2f656c36f12abb806ede6
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2025-08-28 00:31:24 +0200

    ete4 doesn't use the format=8 parameter anymore

--- crest4/databases.py
+++ crest4/databases.py
@@ -258,7 +258,7 @@ class CrestDatabase:
         """
         # Load the tree with ete3 #
         from ete4 import Tree
-        return Tree(self.path.replace_extension('tre'), format=8)
+        return Tree(self.path.replace_extension('tre'))
 
     @property_cached
     def node_to_name(self):
